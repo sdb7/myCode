8ad74d368ea49629ef80d721a98909f7010599ae
sdb <sdb.developer@gmail.com>
2014-05-08 16:16:17 +0700

    Extension for unity 3d rectangle
    Jika ingin bwt extensi lagi tolong diteruskan dari sni saja

---
+++ RectExtension.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public static class RectExtension 
+{
+    public static Rect OffsetRectHorizontal(this Rect value, float padX)
+    {
+        if (padX == 0)
+            padX = 1;
+
+        Rect r = new Rect(value.xMax + padX, value.yMin , value.width, value.height);
+        return r;
+    }
+
+    public static Rect OffsetRectVertical(this Rect value, float padY)
+    {
+        if (padY == 0)
+            padY = 1;
+
+        Rect r = new Rect(value.xMin, value.yMax + padY, value.width, value.height);
+        return r;
+    }
+
+    public static Rect OffsetRect(this Rect value, float padX, float padY)
+    {
+        if (padX == 0)
+            padX = 1;
+
+        if (padY == 0)
+            padY = 1;
+
+        Rect r = new Rect(value.xMax + padX, value.yMax + padY, value.width, value.height);
+        return r;
+    }
+
+    public static Rect[] MultiplyHorizontal(this Rect value, int count, float pad)
+    {
+        Rect[] r = new Rect[count];
+        for (int i = 1; i < r.Length; i++)
+        {
+            r[i] = new Rect(i * (value.yMax + pad), value.yMin, value.width, value.height);
+        }
+
+        return r;
+    }
+}
